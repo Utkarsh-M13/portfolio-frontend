34001e442a64fe243482eaab8cbbf3fdf1ed4946
Utkarsh-M13 <142945737+Utkarsh-M13@users.noreply.github.com>
2025-08-11 19:18:48 +0530

    add music ui

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getLastPlayed } from "../services/spotify"
 
 type Props  = {
@@ -6,22 +6,29 @@ type Props  = {
 }
 
 const Music = ({forwardRef} : Props) => {
+  const [artists, setArtists] = useState("")
+  const [song, setSong] = useState("")
+  const [src, setSrc] = useState("")
+
   useEffect(() => {
     const handleFetch = async () => {
       const lastPlayedSong = await getLastPlayed()
       console.log('lastPlayedSong', lastPlayedSong)
+      setArtists(lastPlayedSong.artists)
+      setSong(lastPlayedSong.name)
+      setSrc(lastPlayedSong.cover)
     }
     handleFetch()
   }, [])
   return (
-    <div ref={forwardRef} className='h-[84px] w-[260px] bg-primary border-[#2C2C2C] border-1 flex absolute rounded-lg top-12 left-[-20px] gap-8 cursor-pointer pl-4 items-center'>
+    <div ref={forwardRef} className='h-[84px] w-[260px] bg-primary border-[#2C2C2C] border-1 flex absolute rounded-lg top-12 left-[-20px] gap-8 cursor-pointer pl-4 items-center z-0'>
       <div className='h-14'>
-        <img className='h-full rounded-xl object-contains' src="/assets/avicii.webp" alt="" />
+        <img className='h-full rounded-xl object-contains' src={src} alt="" />
 
       </div>
       <div className='flex flex-col gap-[-4px] font-light text-left'>
-        <span className='text-secondary text-md '>The Nights</span>
-        <span className='text-[#A6A6A6] text-xs'>Avicii</span>
+        <span className='text-secondary text-md '>{song}</span>
+        <span className='text-[#A6A6A6] text-xs'>{artists}</span>
 
       </div>
     </div>
