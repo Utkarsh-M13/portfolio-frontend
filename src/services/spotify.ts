83497a2558878577de2debc4a913c028ca1fbf14
Utkarsh-M13 <142945737+Utkarsh-M13@users.noreply.github.com>
2025-08-11 18:48:00 +0530

    fix music

--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export async function getLastPlayed() {
-  const { data } = await axios.get('/api/spotify/last', { timeout: 5000 });
+  const { data } = await axios.get('/api/last', { timeout: 5000 });
   return data;
 }
